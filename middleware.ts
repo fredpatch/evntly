f2b11320a29ffa74ef39ca629b05f05c0f356848
fredpatch <f.patchelli@gmail.com>
2024-10-15 07:24:18 +0100

    first commit

---
+++ middleware.ts
@@ -0,0 +1,32 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/",
+  "/events/:id",
+]);
+
+const ignoredRoutes = createRouteMatcher([
+  "/api/webhook/clerk",
+  "/api/webhook/stripe",
+  "/api/uploadthing",
+]);
+
+export default clerkMiddleware(
+  (auth, request) => {
+    if (!isPublicRoute(request)) {
+      auth().protect();
+    }
+  },
+  { debug: false }
+);
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
