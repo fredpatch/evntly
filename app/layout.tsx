0e52645f05eecd4d28564c4c4bb2fce8ea0d8f86
fredpatch <f.patchelli@gmail.com>
2024-10-15 19:06:35 +0100

    fix redirect

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,12 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider
-      signUpFallbackRedirectUrl={"/"}
-      signInFallbackRedirectUrl={"/"}
-      signInForceRedirectUrl={"/"}
+      signInForceRedirectUrl={
+        process.env.NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL
+      }
+      signUpForceRedirectUrl={
+        process.env.NEXT_PUBLIC_CLERK_SIGN_UP_FALLBACK_REDIRECT_URL
+      }
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
     >
       <html lang="en">
