e0849c31a688cdf0f5002e309ad2ef7fa2b0d418
fredpatch <f.patchelli@gmail.com>
2024-10-15 16:47:17 +0100

    deploy

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -2,7 +2,7 @@ import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
-import { clerkClient } from "@clerk/nextjs/server";
+import { clerkClient } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
@@ -63,8 +63,8 @@ export async function POST(req: Request) {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name ?? "",
-      lastName: last_name ?? "",
+      firstName: first_name!,
+      lastName: last_name!,
       photo: image_url,
     };
 
--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
-  "/sign-in(.*)",
-  "/sign-up(.*)",
   "/",
   "/events/:id",
+  "/api/webhook/clerk",
+  "/api/webhook/stripe",
+  "/api/uploadthing",
 ]);
 
 const ignoredRoutes = createRouteMatcher([
@@ -13,20 +14,12 @@ const ignoredRoutes = createRouteMatcher([
   "/api/uploadthing",
 ]);
 
-export default clerkMiddleware(
-  (auth, request) => {
-    if (!isPublicRoute(request)) {
-      auth().protect();
-    }
-  },
-  { debug: false }
-);
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
+});
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
