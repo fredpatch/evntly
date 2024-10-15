32acbefee0bc11c22aa82d0c8820ec61856e03ac
fredpatch <f.patchelli@gmail.com>
2024-10-15 17:47:36 +0100

    fixc

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider signInFallbackRedirectUrl={"/"}>
       <html lang="en">
         <body className={`${poppins.variable} ${poppins.variable} antialiased`}>
           {children}
